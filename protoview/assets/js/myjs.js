235547dab246ad05d2fce6fe62d007527f27543f
rbayu505 <restabayu.s@gmail.com>
2020-02-11 16:19:54 +0700

    initial commit

--- protoview/assets/js/myjs.js
+++ protoview/assets/js/myjs.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
- /*
-        ################
-        Add navbar background color when scrolled
-        */
+
+      //Navbar header section
+
+      /* Add navbar background color when scrolled */
        $(window).scroll(function() {
         if ($(window).scrollTop() > 56) {
           $(".navbar").addClass("bg-bpd");
@@ -10,7 +10,7 @@ $(document).ready(function() {
           $(".navbar").removeClass("bg-bpd");
         }
       });
-      // If Mobile, add background color when toggler is clicked
+      /* If Mobile, add background color when toggler is clicked */
       $(".navbar-toggler").click(function() {
         if (!$(".navbar-collapse").hasClass("show")) {
           $(".navbar").addClass("bg-bpd");
@@ -21,5 +21,31 @@ $(document).ready(function() {
           }
         }
       });
-      // ############
+      // #####
+
+      // Card Slider Section
+      $("#myCarousel").on("slide.bs.carousel", function(e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 4;
+        var totalItems = $(".carousel-item").length;
+    
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+          var it = itemsPerSlide - (totalItems - idx);
+          for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "left") {
+              $(".carousel-item")
+                .eq(i)
+                .appendTo(".carousel-inner");
+            } else {
+              $(".carousel-item")
+                .eq(0)
+                .appendTo($(this).find(".carousel-inner"));
+            }
+          }
+        }
+      });
+      // #####
+
     });
