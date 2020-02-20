2f78c3949239f04d6e084ace1f7895039a9112e7
rbayu505 <restabayu.s@gmail.com>
2020-02-20 09:45:21 +0700

    sementara lp

--- protoview/assets/js/myjs.js
+++ protoview/assets/js/myjs.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function() 
+  {
 
       //Navbar header section
 
@@ -34,28 +35,54 @@ $(document).ready(function() {
       // #####
 
       // Card Slider Section
-      $("#carousel-2").on("slide.bs.carousel", function(e) {
-        var $e = $(e.relatedTarget);
-        var idx = $e.index();
-        var itemsPerSlide = 4;
-        var totalItems = $(".carousel-item").length;
+      // $("#carousel-2").on("slide.bs.carousel", function(e) {
+      //   var $e = $(e.relatedTarget);
+      //   var idx = $e.index();
+      //   var itemsPerSlide = 4;
+      //   var totalItems = $(".carousel-item").length;
     
-        if (idx >= totalItems - (itemsPerSlide - 1)) {
-          var it = itemsPerSlide - (totalItems - idx);
-          for (var i = 0; i < it; i++) {
-            // append slides to end
-            if (e.direction == "left") {
-              $(".carousel-item")
-                .eq(i)
-                .appendTo(".carousel-inner");
-            } else {
-              $(".carousel-item")
-                .eq(0)
-                .appendTo($(this).find(".carousel-inner"));
-            }
-          }
-        }
-      });
+      //   if (idx >= totalItems - (itemsPerSlide - 1)) {
+      //     var it = itemsPerSlide - (totalItems - idx);
+      //     for (var i = 0; i < it; i++) {
+      //       // append slides to end
+      //       if (e.direction == "left") {
+      //         $(".carousel-item")
+      //           .eq(i)
+      //           .appendTo(".carousel-inner");
+      //       } else {
+      //         $(".carousel-item")
+      //           .eq(0)
+      //           .appendTo($(this).find(".carousel-inner"));
+      //       }
+      //     }
+      //   }
+      // });
       // #####
 
+      // OWL Carousel
+      $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText: [ "<i class=\"fa fa-chevron-left\"></i>",
+             "<i class=\"fa fa-chevron-right\"></i>" ],
+        dots:false,
+        smartSpeed:500,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
     });
+    // #####
+
+  });
