e4f4864ae30c741a3d920e2c08747bef71340d17
rbayu505 <restabayu.s@gmail.com>
2020-02-21 10:24:35 +0700

    foot

--- protoview/assets/js/myjs.js
+++ protoview/assets/js/myjs.js
@@ -84,31 +84,31 @@ $(document).ready(function()
         }
     });
 
-    $('#owl-carousel2').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      navText: [ "<i class=\"fa fa-chevron-left\"></i>",
-          "<i class=\"fa fa-chevron-right\"></i>" ],
-      dots:false,
-      smartSpeed:500,
-      autoplay:true,
-      autoplayTimeout:3000,
-      autoplayHoverPause:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:3
-          },
-          1000:{
-              items:5
-          }
-      }
-  });
+      $('#owl-carousel2').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText: [ "<i class=\"fa fa-chevron-left\"></i>",
+            "<i class=\"fa fa-chevron-right\"></i>" ],
+        dots:false,
+        smartSpeed:500,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
     // #####
-
+                        
   });
     
     
